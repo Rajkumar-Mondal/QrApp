e188ebe981121582c330a2566f80e94567dcacaf
Rajkumar Mondal <rajkumar.mondal@varatechnology.com>
2022-07-19 17:40:25 +0530

    Feature: Generate Qr Code Complete

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Type here to translate to Qr code!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      {text ?
+        <QRCode
+          value={text}
+        /> : null}
     </View>
   );
 }
@@ -17,4 +30,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 5,
+    padding: 5,
+  }
 });
