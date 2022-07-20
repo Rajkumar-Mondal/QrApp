8eb29b125cbb2ca29a70ed443d0bf472be81c4c8
Rajkumar Mondal <rajkumar.mondal@varatechnology.com>
2022-07-20 17:47:44 +0530

    Feature: Add Scanning Qr Code in App

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TextInput } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
+import React, { useState, useEffect } from "react";
+import { Text, TextInput, View, StyleSheet, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import QRCode from "react-native-qrcode-svg";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
@@ -12,13 +34,22 @@ export default function App() {
       <TextInput
         style={styles.input}
         placeholder="Type here to translate to Qr code!"
-        onChangeText={newText => setText(newText)}
+        onChangeText={(newText) => setText(newText)}
         defaultValue={text}
       />
-      {text ?
-        <QRCode
-          value={text}
-        /> : null}
+      {text ? <QRCode value={text} /> : null}
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        {scanned && (
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
+        )}
+      </View>
     </View>
   );
 }
@@ -26,16 +57,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     margin: 10,
     borderRadius: 5,
     padding: 5,
-  }
+  },
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 200,
+    overflow: "hidden",
+    borderRadius: 2,
+    backgroundColor: "white",
+  },
 });
